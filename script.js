44b6791a25a8a10bbb64c7cfb19e1a945f32410b
Arbab Ahamad <ahmadarbab.456@gmail.com>
2026-06-14 22:52:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 // your code here
+const nameInput = document.getElementById("name");
+const yearInput = document.getElementById("year");
+const button = document.getElementById("button");
+const urlHeading = document.getElementById("url");
+
+button.addEventListener("click", () => {
+	const name = nameInput.value.trim();
+	const year = yearInput.value.trim();
+
+	let params = [];
+
+	if(name) {
+		params.push(`name=${encodeURIComponent(name)}`);
+	}
+
+	if(year) {
+		params.push(`year=${encodeURIComponent(year)}`);
+	}
+
+	let url = "https://localhost:8080/";
+
+	if(params.length > 0) {
+		url += "?" + params.join("&");
+	}
+
+	urlHeading.textContent = url;
+})
